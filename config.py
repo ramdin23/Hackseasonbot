e73a493732fe610945ebb0494c9ea9d0c56c4906
vishnuvaishnav <134823485+vishnuvaishnav@users.noreply.github.com>
2023-08-17 15:17:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
+    API_ID = 22500719
+    API_HASH = "be0327edf200116b39eee7bd89582792"
+    TOKEN = "6382999152:AAGlOB1byZ7nPWewbDa41X0L1NIA3z-wXwY"  
+    START_PIC = "https://telegra.ph/file/52ae6da11453aa739421f.jpg" 
     CHAT = ""    
