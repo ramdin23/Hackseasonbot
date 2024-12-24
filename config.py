253e85636b903953e3a321d95a34e88b8b08ceac
ramdin23 <134823485+ramdin23@users.noreply.github.com>
2024-12-24 22:22:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 22500719
     API_HASH = "be0327edf200116b39eee7bd89582792"
-    TOKEN = "7470772942:AAFMUWGb07Ra058O84JVnGKEIpRGDEMAwRU"  
+    TOKEN = "7185157515:AAHT-zYxWtQl-nGZM0yYi7IumyEoE7a4WTQ"  
     START_PIC = "https://telegra.ph/file/52ae6da11453aa739421f.jpg" 
     CHAT = ""    
